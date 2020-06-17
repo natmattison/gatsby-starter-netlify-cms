0ab15f6065d292c2d972cc42ffba69902376773c
natmat <natmattison@gmail.com>
2020-06-17 19:23:35 -0400

    make a bit resilient

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,10 @@ import GlobalStyles from "../../styles/GlobalStyles"
 import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-167215828-1');
-ReactGA.pageview(window.location.pathname);
+
+if(window && window.location) {
+  ReactGA.pageview(window.location.pathname);
+}
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
